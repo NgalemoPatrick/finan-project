28e91201b360b37ee1f9f4cdf608e851bfd9531f
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-09-08 11:03:05 -0500

    create routes to handle 404 error

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,18 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.use("/", require("./routes/root"));
 
 app.listen(PORT, () => console.log(`Server listning on port ${PORT}`));
+
+
+// handle route not found
+app.all('*', (req, res) => {
+    res.status(404)
+    // check the request header
+    if(req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    }else if(req.accepts('json')) {
+        res.json({ message: '404 Not Found'})
+    }else {
+        req.type('txt').send('404 Not Found')
+    }
+
+})
