3b97b4661f14c69c89d53f50bbb54186b2b03333
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-09-08 11:43:41 -0500

    update logger.js file

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -1,26 +1,31 @@
-const { format } = require('date-fns')
-const { v4: uuid } = require('uuid')
-const fs = require('fs')
-const fsPromises = require('fs').promises
-const path = require('path')
-
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
 
 // helper function to create events
 const logEvents = async (message, logFileName) => {
-    const dateTime = `${format( new Date(), "yyyyMMdd\tHH:mm:ss")}`
-    const logItem = `${ dateTime }\t${uuid}\t${message}`
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
-    try {
-        if(!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
-            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
-        }
-        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logFileName), logItem)
-    } catch (error) {
-        console.log(error)
+  try {
+    if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
     }
-}
+    await fsPromises.appendFile(
+      path.join(__dirname, "..", "logs", logFileName),
+      logItem
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const looger = (req, res, next) => {
-    logEvents(`${req.method}\t${req.url}\t${req.header.origin}`, 'reqLog.log')
-}
+const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
+  console.log(`${req.method}\t${req.path}`);
+  next();
+};
 
+module.exports = { logger, logEvents };
--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,31 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
 const PORT = process.env.PORT || 1337;
 
+// middleware call
+app.use(logger);
 // Process json receive and parse json file
-app.use(express.json())
+app.use(express.json());
 
-
-// handle statis files
-app.use(express.static('public'));
+// handle static files
+app.use(express.static("public"));
 
 // routes
 app.use("/", require("./routes/root"));
 
 app.listen(PORT, () => console.log(`Server listning on port ${PORT}`));
 
-
 // handle route not found
-app.all('*', (req, res) => {
-    res.status(404)
-    // check the request header
-    if(req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    }else if(req.accepts('json')) {
-        res.json({ message: '404 Not Found'})
-    }else {
-        req.type('txt').send('404 Not Found')
-    }
-
-})
+app.all("*", (req, res) => {
+  res.status(404);
+  // check the request header
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    req.type("txt").send("404 Not Found");
+  }
+});
